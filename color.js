74c1a97b487bf3d326ae76eb50a1c9fdbc2ed2e5
anton41k <anton12petrenko@gmail.com>
2021-07-07 20:26:17 +0300

    ok

--- color.js
+++ color.js
@@ -6,44 +6,31 @@ const colors = [
   '#009688',
   '#795548',
 ];
-
 const ref = {
     startBtn: document.querySelector('button[data-action="start"]'),
     stopBtn: document.querySelector('button[data-action="stop"]'),
     body: document.querySelector('body')
 };
-
-class ColorCwitch{
-    constructor() {
-        this.isActive = false;
-        this.intervalId = null;
-    }
-    start() {
-        if (this.isActive) {
-            return
-        }
-        this.isActive = true;
-        this.intervalId = setInterval(() => {
-            const randomIndex = randomIntegerFromInterval(0, colors.length - 1);
-            setColorBody(randomIndex);
-        }, 1000);
-    }
-    stop() {
-        clearInterval(this.intervalId);
-        this.isActive = false;
-    }
-};
-
-const colorSwitch = new ColorCwitch();
-
-
-ref.startBtn.addEventListener('click', colorSwitch.start.bind(colorSwitch));
-ref.stopBtn.addEventListener('click', colorSwitch.stop.bind(colorSwitch));
-
-
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+let intervalId = null;
+
+function start() {
+    ref.startBtn.disabled = 1;
+    intervalId = setInterval(() => {
+        const randomIndex = randomIntegerFromInterval(0, colors.length - 1);
+        setColorBody(randomIndex);
+    }, 1000);
+}
+
+function stop() {
+    clearInterval(intervalId);
+    ref.startBtn.disabled = 0;
+}
+
+ref.startBtn.addEventListener('click', start);
+ref.stopBtn.addEventListener('click', stop);
 
 function setColorBody(index) {
     ref.body.style.backgroundColor = colors[index];
